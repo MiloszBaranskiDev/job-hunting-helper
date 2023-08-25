3fe3260f8b115c1abf5d5cdd245f868800951a11
Miłosz Barański <miloszbaranski@outlook.com>
2023-08-25 23:17:39 +0200

    add server router lib

---
+++ libs/jhh-server/router/src/lib/jhh-server-router.spec.ts
@@ -0,0 +1,7 @@
+import { jhhServerRouter } from './jhh-server-router';
+
+describe('jhhServerRouter', () => {
+  it('should work', () => {
+    expect(jhhServerRouter()).toEqual('jhh-server-router');
+  });
+});
