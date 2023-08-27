1365b1d96f2541566fc0fc790f9d39a0a9221561
Miłosz Barański <miloszbaranski@outlook.com>
2023-08-27 02:54:46 +0200

    remove

--- libs/jhh-server/router/src/lib/jhh-server-router.spec.ts
+++
@@ -1,7 +0,0 @@
-import { jhhServerRouter } from './jhh-server-router';
-
-describe('jhhServerRouter', () => {
-  it('should work', () => {
-    expect(jhhServerRouter()).toEqual('jhh-server-router');
-  });
-});
