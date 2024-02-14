3351f24adad5d705ea800618e9f1448344330864
MiloszBaranskiDev <miloszbaranski@outlook.com>
2024-02-14 23:14:48 +0100

    refactor(jhh-client): auth ui template
    - move parts to child components

---
+++ libs/jhh-client/auth/ui-template/src/lib/components/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,42 @@
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BehaviorSubject, tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
+import { ThemeService } from '@jhh/jhh-client/shared/util-theme';
+
+import { ThemeMode } from '@jhh/jhh-client/shared/domain';
+
+@Component({
+  selector: 'jhh-auth-theme-switcher',
+  standalone: true,
+  imports: [CommonModule, MatSlideToggleModule, MatTooltipModule],
+  templateUrl: './theme-switcher.component.html',
+  styleUrls: ['./theme-switcher.component.scss'],
+})
+export class ThemeSwitcherComponent implements OnInit {
+  private readonly destroyRef: DestroyRef = inject(DestroyRef);
+  private readonly themeService: ThemeService = inject(ThemeService);
+
+  currentThemeMode$: BehaviorSubject<ThemeMode>;
+
+  isDarkMode: boolean;
+
+  ngOnInit(): void {
+    this.currentThemeMode$ = this.themeService.currentThemeMode$;
+    this.currentThemeMode$
+      .pipe(
+        tap((val) => {
+          this.isDarkMode = val === ThemeMode.Dark;
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe();
+  }
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
+}
--- libs/jhh-client/auth/ui-template/src/lib/containers/template/jhh-client-auth-ui-template.component.ts
+++ libs/jhh-client/auth/ui-template/src/lib/containers/template/jhh-client-auth-ui-template.component.ts
@@ -1,92 +1,24 @@
-import { Component, DestroyRef, inject, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { BehaviorSubject, tap } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
-import { ThemeService } from '@jhh/jhh-client/shared/util-theme';
-
-import { ThemeMode } from '@jhh/jhh-client/shared/domain';
-
-interface Icon {
-  svgSrc: string;
-  name: string;
-  tooltip: string;
-  link: string;
-}
+import { ThemeSwitcherComponent } from '../../components/theme-switcher/theme-switcher.component';
+import { IconsComponent } from '../../components/icons/icons.component';
+import { ContentComponent } from '../../components/content/content.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
-  selector: 'jhh-auth-ui-template',
+  selector: 'jhh-auth-template',
   standalone: true,
   imports: [
     CommonModule,
-    MatIconModule,
-    MatDividerModule,
-    MatSlideToggleModule,
-    MatTooltipModule,
+    ThemeSwitcherComponent,
+    IconsComponent,
+    ContentComponent,
+    HeaderComponent,
   ],
   templateUrl: './jhh-client-auth-ui-template.component.html',
   styleUrls: ['./jhh-client-auth-ui-template.component.scss'],
 })
-export class JhhClientAuthUiTemplateComponent implements OnInit {
-  private readonly destroyRef: DestroyRef = inject(DestroyRef);
-  private readonly matIconRegistry: MatIconRegistry = inject(MatIconRegistry);
-  private readonly domSanitizer: DomSanitizer = inject(DomSanitizer);
-  private readonly themeService: ThemeService = inject(ThemeService);
-
+export class JhhClientAuthUiTemplateComponent {
   @Input() heading: string;
-
-  currentThemeMode$: BehaviorSubject<ThemeMode>;
-
-  isDarkMode: boolean;
-  icons: Icon[] = [
-    {
-      svgSrc: 'assets/icons/github.svg',
-      name: 'github',
-      tooltip: 'GitHub',
-      link: 'https://github.com/MiloszBaranskiDev/job-hunting-helper',
-    },
-    {
-      svgSrc: 'assets/icons/linkedin.svg',
-      name: 'linkedin',
-      tooltip: 'Linkedin',
-      link: 'https://www.linkedin.com/in/mi%C5%82osz-bara%C5%84ski-8617721b1/',
-    },
-    {
-      svgSrc: 'assets/icons/email.svg',
-      name: 'email',
-      tooltip: 'Contact',
-      link: 'https://miloszbaranskidev.github.io/my-website/',
-    },
-  ];
-
-  ngOnInit(): void {
-    this.registerIcons();
-    this.currentThemeMode$ = this.themeService.currentThemeMode$;
-    this.currentThemeMode$
-      .pipe(
-        tap((val) => {
-          this.isDarkMode = val === ThemeMode.Dark;
-        }),
-        takeUntilDestroyed(this.destroyRef)
-      )
-      .subscribe();
-  }
-
-  toggleTheme(): void {
-    this.themeService.toggleTheme();
-  }
-
-  private registerIcons(): void {
-    this.icons.forEach((icon: Icon) => {
-      this.matIconRegistry.addSvgIcon(
-        icon.name,
-        this.domSanitizer.bypassSecurityTrustResourceUrl(icon.svgSrc)
-      );
-    });
-  }
 }
