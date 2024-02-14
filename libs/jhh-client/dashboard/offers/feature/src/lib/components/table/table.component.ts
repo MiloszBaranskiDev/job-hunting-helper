635f40640a81ac33796f4c732767e54d090e8491
MiloszBaranskiDev <miloszbaranski@outlook.com>
2024-02-14 01:38:34 +0100

    refactor(jhh-client): offers table
    - simplify and reduce existing code
    - rearrange order of functions

--- libs/jhh-client/dashboard/offers/feature/src/lib/components/table/table.component.ts
+++ libs/jhh-client/dashboard/offers/feature/src/lib/components/table/table.component.ts
@@ -30,20 +30,23 @@ import { RouterLink } from '@angular/router';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatButtonModule } from '@angular/material/button';
-import { Observable, tap } from 'rxjs';
+import { filter, Observable, tap } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
 
 import { MenuComponent } from '../menu/menu.component';
-
-import { Offer, OfferPriority, OfferStatus } from '@jhh/shared/domain';
-import { OffersPerPage } from '@jhh/jhh-client/dashboard/offers/domain';
 
 import { RemoveOffersDialogService } from '@jhh/jhh-client/dashboard/offers/feature-remove-offers';
 import { OffersFacade } from '@jhh/jhh-client/dashboard/offers/data-access';
 import { QueryParamsService } from '../../services/query-params/query-params.service';
 import { FormatOfferSalaryPipe } from '@jhh/jhh-client/dashboard/offers/util-format-offer-salary';
 import { GetOfferStatusIcon } from '@jhh/jhh-client/dashboard/offers/util-get-offer-status-icon';
+
+import { Offer, OfferPriority, OfferStatus } from '@jhh/shared/domain';
+import {
+  OffersPerPage,
+  OffersTableColumn,
+} from '@jhh/jhh-client/dashboard/offers/domain';
 
 interface OfferWithIcon extends Offer {
   statusIcon: string;
@@ -95,19 +98,13 @@ export class TableComponent implements OnInit, AfterViewInit, OnChanges {
   paginatorPage: number;
   paginatorSize: number;
 
+  readonly offersTableColumn: typeof OffersTableColumn = OffersTableColumn;
+  readonly offersTableColumnValues: OffersTableColumn[] =
+    Object.values(OffersTableColumn);
   readonly offersPerPageValues: number[] = Object.values(OffersPerPage).filter(
     (value): value is number => typeof value === 'number'
   );
-  readonly displayedColumns: string[] = [
-    'select',
-    'position',
-    'company',
-    'location',
-    'salary',
-    'status',
-    'priority',
-    'actions',
-  ];
+
   readonly priorityMapping: { [key: string]: number } = {
     [OfferPriority.High]: 3,
     [OfferPriority.Medium]: 2,
@@ -144,6 +141,7 @@ export class TableComponent implements OnInit, AfterViewInit, OnChanges {
     if (changes['offers']) {
       this.updateDataSource();
       this.updateTableSettings();
+      this.selection.clear();
 
       if (this.paginator) {
         const totalItems: number = this.dataSource.data.length;
@@ -162,141 +160,179 @@ export class TableComponent implements OnInit, AfterViewInit, OnChanges {
   }
 
   applyFilter(event: Event | string): void {
-    const filterValue: string =
+    const filterValue: string = (
       typeof event === 'string'
         ? event
-        : (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+        : (event.target as HTMLInputElement).value
+    )
+      .trim()
+      .toLowerCase();
+    this.dataSource.filter = filterValue;
     this.queryParamsService.updateCurrentFilter(filterValue);
+    this.dataSource.paginator?.firstPage();
+  }
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+  removeSelectedOffers(): void {
+    if (this.selection.selected.length) {
+      this.removeOffersDialogService.openDialog(this.selection.selected);
     }
-  }
-
-  areAllSelected(): boolean {
-    const startIndex: number = this.paginatorPage * this.paginatorSize;
-    const endIndex: number = startIndex + this.paginatorSize;
-    const currentPageData: Offer[] = this.dataSource.data.slice(
-      startIndex,
-      endIndex
-    );
-
-    const numSelected: number = currentPageData.filter((row) =>
-      this.selection.isSelected(row)
-    ).length;
-    const numRows: number = currentPageData.length;
-
-    return numSelected === numRows;
-  }
-
-  areAllSelectedOnCurrentPage(): boolean {
-    const filteredData: Offer[] = this.dataSource.filteredData;
-    const startIndex = this.paginatorPage * this.paginatorSize;
-    const endIndex = startIndex + this.paginatorSize;
-    const currentPageData = filteredData.slice(startIndex, endIndex);
-
-    const numSelected = currentPageData.filter((row) =>
-      this.selection.isSelected(row)
-    ).length;
-    return currentPageData.length > 0 && numSelected === currentPageData.length;
   }
 
   isSomeSelected(): boolean {
     return this.selection.selected.length > 0 && !this.areAllSelected();
   }
 
+  areAllSelected(): boolean {
+    const startIndex: number = this.paginatorPage * this.paginatorSize;
+    const currentPageData: OfferWithIcon[] = this.dataSource.data.slice(
+      startIndex,
+      startIndex + this.paginatorSize
+    );
+
+    return (
+      currentPageData.length > 0 &&
+      currentPageData.every((row) => this.selection.isSelected(row))
+    );
+  }
+
+  areAllSelectedOnCurrentPage(): boolean {
+    const startIndex: number = this.paginatorPage * this.paginatorSize;
+    const currentPageData: OfferWithIcon[] = this.dataSource.filteredData.slice(
+      startIndex,
+      startIndex + this.paginatorSize
+    );
+
+    return (
+      currentPageData.length > 0 &&
+      currentPageData.every((row) => this.selection.isSelected(row))
+    );
+  }
+
   toggleAllOnCurrentPage(): void {
-    const filteredData: Offer[] = this.dataSource.filteredData;
-    const startIndex = this.paginatorPage * this.paginatorSize;
-    const endIndex = startIndex + this.paginatorSize;
-    const currentPageData = filteredData.slice(startIndex, endIndex);
+    const startIndex: number = this.paginatorPage * this.paginatorSize;
+    const currentPageData: OfferWithIcon[] = this.dataSource.filteredData.slice(
+      startIndex,
+      startIndex + this.paginatorSize
+    );
+    const areAllSelected: boolean = this.areAllSelectedOnCurrentPage();
+    const action = areAllSelected
+      ? this.selection.deselect.bind(this.selection)
+      : this.selection.select.bind(this.selection);
 
-    if (this.areAllSelectedOnCurrentPage()) {
-      currentPageData.forEach((row) => this.selection.deselect(row));
-    } else {
-      currentPageData.forEach((row) => this.selection.select(row));
-    }
+    currentPageData.forEach((row) => action(row));
   }
 
-  toggleCheckbox(row: Offer): void {
-    this.selection.toggle(row);
-  }
-
-  removeSelectedOffers(): void {
-    const selectedOffers: Offer[] = this.selection.selected;
-    if (selectedOffers.length > 0) {
-      this.removeOffersDialogService.openDialog(selectedOffers);
-    }
-  }
-
-  handleSort(sortState: Sort): void {
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-
-    const { active, direction } = sortState;
+  handleSort({ active, direction }: Sort): void {
+    this.dataSource.paginator?.firstPage();
     this.queryParamsService.updateCurrentSort(
-      `${active && direction !== '' ? active : ''},${direction || ''}`
+      `${direction ? active : ''},${direction}`
     );
   }
 
   handlePaginator(event: PageEvent): void {
-    if (event.pageIndex !== event.previousPageIndex) {
+    event.pageIndex !== event.previousPageIndex &&
       this.queryParamsService.updateCurrentPage(event.pageIndex + 1);
-    }
-
-    if (event.pageSize !== this.paginatorSize) {
+    event.pageSize !== this.paginatorSize &&
       this.queryParamsService.updateCurrentPerPage(event.pageSize);
-    }
   }
 
-  stopPropagation(event: Event): void {
-    event.stopPropagation();
+  private setSortingDataAccessor(): void {
+    this.dataSource.sortingDataAccessor = (item, property) => {
+      switch (property) {
+        case OffersTableColumn.Priority:
+          return this.priorityMapping[item.priority];
+        case OffersTableColumn.Status:
+          return this.statusMapping[item.status];
+        case OffersTableColumn.Salary:
+          return this.getSortableSalaryValue(item);
+        default:
+          return (item as any)[property];
+      }
+    };
+  }
+
+  private updateDataSource(): void {
+    if (!this.dataSource) {
+      this.dataSource = new MatTableDataSource<OfferWithIcon>();
+    }
+    this.dataSource.data = this.offers.map(
+      (offer) =>
+        ({
+          ...offer,
+          statusIcon: GetOfferStatusIcon(offer.status),
+        } as OfferWithIcon)
+    );
+  }
+
+  private updateTableSettings(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.setSortingDataAccessor();
+  }
+
+  private isValidSort(active: string, direction: string): boolean {
+    return (
+      this.offersTableColumnValues.includes(active as OffersTableColumn) &&
+      ['asc', 'desc', ''].includes(direction)
+    );
+  }
+
+  private getSortableSalaryValue(item: Offer): number {
+    return item.minSalary && item.maxSalary
+      ? (item.minSalary + item.maxSalary) / 2
+      : item.minSalary || item.maxSalary || 0;
+  }
+
+  private handleRemoveSuccess(): void {
+    this.removeOffersSuccess$
+      .pipe(
+        filter((val) => val),
+        tap(() => {
+          this.selection.clear();
+        }),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe();
   }
 
   private useQueryParams(): void {
     this.queryParamsService
       .getAllQueryParams$()
       .pipe(
-        tap((val) => {
-          this.filterValue = val.filter === 'null' ? '' : val.filter;
+        tap(({ filter, perPage, page, sort }) => {
+          this.filterValue =
+            filter === 'null' || filter === 'undefined' ? '' : filter;
 
-          if (this.offersPerPageValues.includes(val.perPage)) {
-            this.paginatorSize = val.perPage;
-          } else {
-            this.paginatorSize = OffersPerPage.Fifteen;
-            this.queryParamsService.updateCurrentPerPage(OffersPerPage.Fifteen);
+          this.paginatorSize = this.offersPerPageValues.includes(perPage)
+            ? perPage
+            : OffersPerPage.Ten;
+          if (!this.offersPerPageValues.includes(perPage)) {
+            this.queryParamsService.updateCurrentPerPage(OffersPerPage.Ten);
           }
 
-          const totalItems: number = this.dataSource.data.length;
-          const totalPages: number = Math.ceil(totalItems / this.paginatorSize);
-
-          if (
-            !Number.isInteger(val.page) ||
-            val.page < 1 ||
-            val.page > totalPages
-          ) {
-            this.paginatorPage = 1;
+          const totalPages: number = Math.ceil(
+            this.dataSource.data.length / this.paginatorSize
+          );
+          this.paginatorPage =
+            !Number.isInteger(page) || page < 1 || page > totalPages
+              ? 1
+              : page - 1;
+          if (this.paginatorPage + 1 !== page) {
             this.queryParamsService.updateCurrentPage(1);
-          } else {
-            this.paginatorPage = val.page - 1;
           }
 
-          const [active, direction] = val.sort.split(',');
-          if (this.isValidSort(active, direction)) {
-            if (
-              this.sort.active !== active ||
-              this.sort.direction !== direction
-            ) {
-              this.sort.active = active;
-              this.sort.direction = direction as 'asc' | 'desc' | '';
-              this.sort.sortChange.emit({
-                active,
-                direction: direction as SortDirection,
-              });
-            }
-          } else {
+          const [active, direction] = sort.split(',');
+          if (
+            this.isValidSort(active, direction) &&
+            (this.sort.active !== active || this.sort.direction !== direction)
+          ) {
+            this.sort.active = active;
+            this.sort.direction = direction as 'asc' | 'desc' | '';
+            this.sort.sortChange.emit({
+              active,
+              direction: direction as SortDirection,
+            });
+          } else if (!this.isValidSort(active, direction)) {
             this.queryParamsService.updateCurrentSort(',');
             this.sort.active = '';
             this.sort.direction = '';
@@ -310,69 +346,5 @@ export class TableComponent implements OnInit, AfterViewInit, OnChanges {
     this.paginator.pageIndex = this.paginatorPage;
     this.paginator.pageSize = this.paginatorSize;
     this.setSortingDataAccessor();
-  }
-
-  private handleRemoveSuccess(): void {
-    this.removeOffersSuccess$
-      .pipe(
-        tap((val) => {
-          if (val) {
-            this.selection.clear();
-          }
-        }),
-        takeUntilDestroyed(this.destroyRef)
-      )
-      .subscribe();
-  }
-
-  private setSortingDataAccessor(): void {
-    this.dataSource.sortingDataAccessor = (item, property) => {
-      switch (property) {
-        case 'priority':
-          return this.priorityMapping[item.priority];
-        case 'status':
-          return this.statusMapping[item.status];
-        case 'salary':
-          return this.getSortableSalaryValue(item);
-        default:
-          return (item as any)[property];
-      }
-    };
-  }
-
-  private updateTableSettings(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.setSortingDataAccessor();
-  }
-
-  private updateDataSource(): void {
-    this.dataSource = new MatTableDataSource(
-      this.offers.map(
-        (offer) =>
-          ({
-            ...offer,
-            statusIcon: GetOfferStatusIcon(offer.status),
-          } as OfferWithIcon)
-      )
-    );
-  }
-
-  private isValidSort(active: string, direction: string): boolean {
-    const isValidColumn: boolean = this.displayedColumns.includes(active);
-    const isValidDirection: boolean = ['asc', 'desc', ''].includes(direction);
-    return isValidColumn && isValidDirection;
-  }
-
-  private getSortableSalaryValue(item: Offer): number {
-    if (item.minSalary && item.maxSalary) {
-      return (item.minSalary + item.maxSalary) / 2;
-    } else if (item.minSalary) {
-      return item.minSalary;
-    } else if (item.maxSalary) {
-      return item.maxSalary;
-    } else {
-      return 0;
-    }
   }
 }
