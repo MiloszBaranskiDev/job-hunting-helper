31c9a1ae20ba5757ba2c6767691f53a362280e8f
MiloszBaranskiDev <miloszbaranski@outlook.com>
2024-03-03 23:24:26 +0100

    test(jhh-client): not found page unit tests

--- libs/jhh-client/feature-not-found/src/lib/components/info/info.component.spec.ts
+++ libs/jhh-client/feature-not-found/src/lib/components/info/info.component.spec.ts
@@ -1,9 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
 import { InfoComponent } from './info.component';
 
 describe('InfoComponent', () => {
   let component: InfoComponent;
   let fixture: ComponentFixture<InfoComponent>;
+
+  beforeAll(() => {
+    TestBed.initTestEnvironment(
+      BrowserDynamicTestingModule,
+      platformBrowserDynamicTesting()
+    );
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
