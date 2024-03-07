fe3f580be63ea7b08a66b7bcf1657505a374c1ac
MiloszBaranskiDev <miloszbaranski@outlook.com>
2024-03-07 23:05:33 +0100

    test(jhh-server): board controller unit tests

--- libs/jhh-server/controller/board/src/lib/jhh-server-controller-board.spec.ts
+++
@@ -1,7 +0,0 @@
-import { jhhServerControllerBoard } from './jhh-server-controller-board';
-
-describe('jhhServerControllerBoard', () => {
-  it('should work', () => {
-    expect(jhhServerControllerBoard()).toEqual('jhh-server-controller-board');
-  });
-});
