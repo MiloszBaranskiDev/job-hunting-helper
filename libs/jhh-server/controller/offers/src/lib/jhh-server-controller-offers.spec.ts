c2489988d84bb56f68e4087e78b35e3256542470
MiloszBaranskiDev <miloszbaranski@outlook.com>
2024-03-09 00:25:25 +0100

    test(jhh-server): offers controller unit tests

--- libs/jhh-server/controller/offers/src/lib/jhh-server-controller-offers.spec.ts
+++
@@ -1,7 +0,0 @@
-import { jhhServerControllerOffers } from './jhh-server-controller-offers';
-
-describe('jhhServerControllerOffers', () => {
-  it('should work', () => {
-    expect(jhhServerControllerOffers()).toEqual('jhh-server-controller-offers');
-  });
-});
