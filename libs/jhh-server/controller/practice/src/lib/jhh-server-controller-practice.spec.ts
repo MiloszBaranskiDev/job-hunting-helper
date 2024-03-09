9bec14eb9e6b14554c3e92d1cdef21794c77d119
MiloszBaranskiDev <miloszbaranski@outlook.com>
2024-03-09 01:58:01 +0100

    test(jhh-server): practice controller unit tests

--- libs/jhh-server/controller/practice/src/lib/jhh-server-controller-practice.spec.ts
+++
@@ -1,9 +0,0 @@
-import { JhhServerControllerPractice } from './jhh-server-controller-practice';
-
-describe('JhhServerControllerPractice', () => {
-  it('should work', () => {
-    expect(JhhServerControllerPractice()).toEqual(
-      'jhh-server-controller-practice'
-    );
-  });
-});
