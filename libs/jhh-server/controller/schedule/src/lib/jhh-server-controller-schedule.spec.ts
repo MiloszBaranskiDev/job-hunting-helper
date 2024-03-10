4972b783707245792809552c4d3bfda31efcc4dd
MiloszBaranskiDev <miloszbaranski@outlook.com>
2024-03-10 17:09:56 +0100

    test(jhh-server): schedule controller unit tests

--- libs/jhh-server/controller/schedule/src/lib/jhh-server-controller-schedule.spec.ts
+++
@@ -1,9 +0,0 @@
-import { jhhServerControllerSchedule } from './jhh-server-controller-schedule';
-
-describe('jhhServerControllerSchedule', () => {
-  it('should work', () => {
-    expect(jhhServerControllerSchedule()).toEqual(
-      'jhh-server-controller-schedule'
-    );
-  });
-});
