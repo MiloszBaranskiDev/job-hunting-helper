8bf6b2a86cdc0d2ee8dacc3351d1367f55ac7399
MiloszBaranskiDev <miloszbaranski@outlook.com>
2024-03-11 00:45:10 +0100

    test(jhh-client): notes unit tests
    - remove multi 'describe'

--- libs/jhh-client/dashboard/notes/data-access/src/lib/+state/notes.selectors.spec.ts
+++ libs/jhh-client/dashboard/notes/data-access/src/lib/+state/notes.selectors.spec.ts
@@ -43,184 +43,143 @@ describe('Notes Selectors Full Suite', () => {
     } as any,
   };
 
-  describe('selectEditNotesGroupInProgress', () => {
-    it('should select the editNotesGroup inProgress state', () => {
-      const result = NotesSelectors.selectEditNotesGroupInProgress.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(false);
-    });
+  it('should select the editNotesGroup inProgress state', () => {
+    const result = NotesSelectors.selectEditNotesGroupInProgress.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(false);
   });
 
-  describe('selectEditNotesGroupError', () => {
-    it('should select the editNotesGroupError', () => {
-      const result = NotesSelectors.selectEditNotesGroupError.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe('Error');
-    });
+  it('should select the editNotesGroupError', () => {
+    const result = NotesSelectors.selectEditNotesGroupError.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe('Error');
   });
 
-  describe('selectEditNotesGroupSuccess', () => {
-    it('should select the editNotesGroupSuccess', () => {
-      const result = NotesSelectors.selectEditNotesGroupSuccess.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(false);
-    });
+  it('should select the editNotesGroupSuccess', () => {
+    const result = NotesSelectors.selectEditNotesGroupSuccess.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(false);
   });
 
-  describe('selectDuplicateNotesGroupInProgress', () => {
-    it('should select the duplicateNotesGroupInProgress state', () => {
-      const result =
-        NotesSelectors.selectDuplicateNotesGroupInProgress.projector(
-          mockInitialState.notes
-        );
-      expect(result).toBe(true);
-    });
+  it('should select the duplicateNotesGroupInProgress state', () => {
+    const result = NotesSelectors.selectDuplicateNotesGroupInProgress.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(true);
   });
 
-  describe('selectDuplicateNotesGroupError', () => {
-    it('should select the duplicateNotesGroupError', () => {
-      const result = NotesSelectors.selectDuplicateNotesGroupError.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBeNull();
-    });
+  it('should select the duplicateNotesGroupError', () => {
+    const result = NotesSelectors.selectDuplicateNotesGroupError.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBeNull();
   });
 
-  describe('selectRemoveNotesGroupInProgress', () => {
-    it('should select the removeNotesGroupInProgress state', () => {
-      const result = NotesSelectors.selectRemoveNotesGroupInProgress.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(false);
-    });
+  it('should select the removeNotesGroupInProgress state', () => {
+    const result = NotesSelectors.selectRemoveNotesGroupInProgress.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(false);
   });
 
-  describe('selectRemoveNotesGroupError', () => {
-    it('should select the removeNotesGroupError', () => {
-      const result = NotesSelectors.selectRemoveNotesGroupError.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBeNull();
-    });
+  it('should select the removeNotesGroupError', () => {
+    const result = NotesSelectors.selectRemoveNotesGroupError.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBeNull();
   });
 
-  describe('selectRemoveNotesGroupSuccess', () => {
-    it('should select the removeNotesGroupSuccess', () => {
-      const result = NotesSelectors.selectRemoveNotesGroupSuccess.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(true);
-    });
+  it('should select the removeNotesGroupSuccess', () => {
+    const result = NotesSelectors.selectRemoveNotesGroupSuccess.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(true);
   });
 
-  describe('selectAddNoteInProgress', () => {
-    it('should select the addNoteInProgress', () => {
-      const result = NotesSelectors.selectAddNoteInProgress.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(false);
-    });
+  it('should select the addNoteInProgress', () => {
+    const result = NotesSelectors.selectAddNoteInProgress.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(false);
   });
 
-  describe('selectAddNoteError', () => {
-    it('should select the addNoteError', () => {
-      const result = NotesSelectors.selectAddNoteError.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe('Add note error');
-    });
+  it('should select the addNoteError', () => {
+    const result = NotesSelectors.selectAddNoteError.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe('Add note error');
   });
 
-  describe('selectAddNoteSuccess', () => {
-    it('should select the addNoteSuccess', () => {
-      const result = NotesSelectors.selectAddNoteSuccess.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(true);
-    });
+  it('should select the addNoteSuccess', () => {
+    const result = NotesSelectors.selectAddNoteSuccess.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(true);
   });
 
-  describe('selectEditNoteInProgress', () => {
-    it('should select the editNoteInProgress state', () => {
-      const result = NotesSelectors.selectEditNoteInProgress.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(true);
-    });
+  it('should select the editNoteInProgress state', () => {
+    const result = NotesSelectors.selectEditNoteInProgress.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(true);
   });
 
-  describe('selectEditNoteError', () => {
-    it('should select the editNoteError', () => {
-      const result = NotesSelectors.selectEditNoteError.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBeNull();
-    });
+  it('should select the editNoteError', () => {
+    const result = NotesSelectors.selectEditNoteError.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBeNull();
   });
 
-  describe('selectEditNoteSuccess', () => {
-    it('should select the editNoteSuccess state', () => {
-      const result = NotesSelectors.selectEditNoteSuccess.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(false);
-    });
+  it('should select the editNoteSuccess state', () => {
+    const result = NotesSelectors.selectEditNoteSuccess.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(false);
   });
 
-  describe('selectChangeNoteGroupInProgress', () => {
-    it('should select the changeNoteGroupInProgress state', () => {
-      const result = NotesSelectors.selectChangeNoteGroupInProgress.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(true);
-    });
+  it('should select the changeNoteGroupInProgress state', () => {
+    const result = NotesSelectors.selectChangeNoteGroupInProgress.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(true);
   });
 
-  describe('selectChangeNoteGroupError', () => {
-    it('should select the changeNoteGroupError', () => {
-      const result = NotesSelectors.selectChangeNoteGroupError.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBeNull();
-    });
+  it('should select the changeNoteGroupError', () => {
+    const result = NotesSelectors.selectChangeNoteGroupError.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBeNull();
   });
 
-  describe('selectChangeNoteGroupSuccess', () => {
-    it('should select the changeNoteGroupSuccess state', () => {
-      const result = NotesSelectors.selectChangeNoteGroupSuccess.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(false);
-    });
+  it('should select the changeNoteGroupSuccess state', () => {
+    const result = NotesSelectors.selectChangeNoteGroupSuccess.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(false);
   });
 
-  describe('selectRemoveNoteInProgress', () => {
-    it('should select the removeNoteInProgress state', () => {
-      const result = NotesSelectors.selectRemoveNoteInProgress.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(false);
-    });
+  it('should select the removeNoteInProgress state', () => {
+    const result = NotesSelectors.selectRemoveNoteInProgress.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(false);
   });
 
-  describe('selectRemoveNoteError', () => {
-    it('should select the removeNoteError', () => {
-      const result = NotesSelectors.selectRemoveNoteError.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe('Remove note error');
-    });
+  it('should select the removeNoteError', () => {
+    const result = NotesSelectors.selectRemoveNoteError.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe('Remove note error');
   });
 
-  describe('selectRemoveNoteSuccess', () => {
-    it('should select the removeNoteSuccess state', () => {
-      const result = NotesSelectors.selectRemoveNoteSuccess.projector(
-        mockInitialState.notes
-      );
-      expect(result).toBe(true);
-    });
+  it('should select the removeNoteSuccess state', () => {
+    const result = NotesSelectors.selectRemoveNoteSuccess.projector(
+      mockInitialState.notes
+    );
+    expect(result).toBe(true);
   });
 });
